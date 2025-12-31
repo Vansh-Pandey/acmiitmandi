74ac52cb0805e1b6d0c4c059b1a1d899aae599b7
Ojasvi310 <179226837+Ojasvi310@users.noreply.github.com>
2026-01-01 02:35:03 +0530

    Updated section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
 import ContactSection from '../components/ContactSection'
+import Footer from '../components/Footer'
 import JoinSection from '../components/JoinSection'
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <AboutSection />
-      <JoinSection/>
+      <JoinSection />
       <ContactSection/>
+      <Footer/>
     </>
   )
 }
